8978dc141044ccf6d12c7cd55b630bf4eec4855d
Namee2810 <62344286+Namee2810@users.noreply.github.com>
2021-01-23 19:03:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 8000;
+const port = process.env.PORT || 80;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
