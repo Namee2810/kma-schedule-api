22ff580a4f763895ff01be7c0f2a21cc4293d3ae
Namee2810 <dvn281002@gmail.com>
2021-04-06 19:24:56 +0700

    test account

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { generateAccessToken } = require('../modules/jwt');
 const router = express.Router();
 const login = require("../modules/login");
+const test = require("../modules/test.json");
+
+const tokenTest = generateAccessToken(test)
 
 router.post('/', (req, res) => {
+  if (req.body.username === "test" && req.body.password === "test")
+    return res.json({
+      status: 200,
+      token: tokenTest
+    });
   login(req.body.username, req.body.password)
     .then(response => {
       res.json(response);
